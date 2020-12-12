02586d80fa3cfcb80b071a317dbb7243ac9ef645
Steve Thomas <steve@revl.ca>
2020-12-12 12:03:41 -0500

    Fix custom agents

--- custom-agents/build.sh
+++ custom-agents/build.sh
@@ -9,6 +9,8 @@ export $(grep -v '^#' ../.env | xargs -d '\n')
 find -path './*' -type d | while IFS= read -r FILE; do
   TAG=$(basename "$FILE")
 
+  echo "Building $FILE..."
+
   docker image build --build-arg GOCD_VERSION=$GOCD_VERSION -f $FILE/Dockerfile -t $DOCKER_REGISTRY/$TAG:$GOCD_VERSION .
   docker image push $DOCKER_REGISTRY/$TAG:$GOCD_VERSION
 done
